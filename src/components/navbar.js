15b75b2d9b7b4a99dc0d8c74cb5cd55713c3a1e1
codekane <ryan.horricks@gmail.com>
2021-08-07 18:21:15 -0600

    Alterations to prepare for deployment

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,7 @@
 export default function NavBar(props) {
   return(
     <div className="NavBar">
-      <b><span id="Brand" style={{marginLeft: "15px"}}>Dash</span></b>
+      <b><span id="Brand" style={{marginLeft: "15px"}}>Dashboard</span></b>
 
     </div>
   )
