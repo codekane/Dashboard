15b40669d6dcdc2523025f1beaf9048a4e061dec
codekane <ryan.horricks@gmail.com>
2021-09-09 14:13:49 -0600

    Preliminary work on updated navigation feature on board. It's not complete, but I needed to make a commit to demonstrate how this is done, so I'm doing this now. Needs work.

--- src/components/board/DashBoardNav.js
+++ src/components/board/DashBoardNav.js
@@ -1,10 +1,27 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faRecycle } from '@fortawesome/free-solid-svg-icons';
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 
 export default function DashBoardNav(props) {
   const cards = useSelector(state => state.cards);
   const activeBoard = useSelector(state => state.activeBoard);
+  const activeBoardTitle = useSelector(state => state.boards[activeBoard].title);
+
+  const [menuShown, setMenuShown] = useState(false);
+
+  const showMenu = (event) => {
+    event.preventDefault();
+    setMenuShown(true);
+    document.addEventListener('click', closeMenu);
+    console.log(menuShown);
+  }
+
+  const closeMenu = () => {
+    setMenuShown(false);
+    document.removeEventListener('click', closeMenu);
+  }
+
   const getCards = () => {
     let out = []
     Object.keys(cards).map(key =>  out.push(cards[key]) );
@@ -25,6 +42,22 @@ export default function DashBoardNav(props) {
   return (
     <div className="DashBoardNav">
       <p>{discardedCards().length}     <FontAwesomeIcon icon={faRecycle} style={{color: "#43F7C8"}}/></p>
+
+
+      <div onClick={showMenu}>
+        <p>{activeBoardTitle} <strong>Dashboard</strong></p>
+        {
+          menuShown ? (
+            <div className="menu">
+              <div>Test1</div>
+              <div>Test2</div>
+              <div>Test3</div>
+            </div>
+
+          ) : ( null )
+        }
+      </div>
+
       <p>{completedCards().length}     <FontAwesomeIcon icon={faStar} style={{color: "gold"}}/></p>
     </div>
   )
